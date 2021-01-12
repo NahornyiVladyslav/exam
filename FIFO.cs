ba9aef4aba04ea85c86aebc9a684a7fdf7958d8a
NahornyiVladyslav <panda.san.nag@gmail.com>
2021-01-12 14:05:24 +0200

    first commit

---
+++ FIFO.cs
@@ -0,0 +1,61 @@
+﻿namespace ExamTask
+{
+    public class FIFO
+    {
+        private object locker = new object();
+        private int[] array;
+        private int length;
+        public FIFO(int size)
+        {
+            array = new int[size];
+        }
+
+        public int Count()
+        {
+            return length;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public bool TryDequeue(out int value)
+        {
+            lock (locker)
+            {
+                if (length > array.Length || length == 0)
+                {
+                    value = default;
+                    return false;
+                }
+                else
+                {
+                    value = array[length - 1];
+                    array[length - 1] = 0;
+                    length--;
+                    return true;
+                }
+            }
+        }
+
+        public void Enqueue(int item)
+        {
+            lock (locker)
+            {
+                if (length != array.Length)
+                {
+                    for (int i = length - 1; i >= 0; i--)
+                    {
+                        array[i + 1] = array[i];
+                    }
+                    array[0] = item;
+                    length++;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExamTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out var size);
+            var queue = new FIFO(size);
+            Task.Run(() => Enqueue(queue));
+            Task.Run(() => Dequeue(queue));
+            Task.Run(() => EnqueueAndDequeue(queue));
+            Task.Run(() => EnqueueAndDequeue(queue));
+            Console.ReadLine();
+            Console.WriteLine(queue.Count());
+            Console.ReadLine();
+        }
+
+        private static void Enqueue(FIFO queue)
+        {
+            for (int i = 0; i < queue.Length; i++)
+            {
+                Console.WriteLine("Enqueue");
+                queue.Enqueue(i);
+            }
+        }
+
+        private static void Dequeue(FIFO queue)
+        {
+            while (true)
+            {
+                var temp = 0;
+                if (queue.TryDequeue(out temp))
+                {
+                    Console.WriteLine("Dequeued: " + temp);
+                }
+            }
+        }
+
+        private static void EnqueueAndDequeue(FIFO queue)
+        {
+            for (int i = 0; i < queue.Length; i++)
+            {
+                Console.WriteLine("Enqueue");
+                queue.Enqueue(i);
+                var temp = 0;
+                if (queue.TryDequeue(out temp))
+                {
+                    Console.WriteLine("Dequeued: " + temp);
+                }
+            }
+        }
+
+    }
+}
